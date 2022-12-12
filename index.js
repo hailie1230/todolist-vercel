6d86ca43e484761e6d70923a790e4dc8b8685c40
Hailie Park <66510673+hailie1230@users.noreply.github.com>
2022-12-12 21:06:36 +0900

    index.js 수정 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-const path = require("path");
+// const path = require("path");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 
 app.use(cors());
 app.use(express.json()); // for parsing application/json
@@ -20,11 +20,11 @@ const todoList = [
   },
 ];
 
-app.get("/api/todo", (req, res) => {
+app.get("/", (req, res) => {
   res.json(todoList);
 });
 
-app.post("/api/todo", (req, res) => {
+app.post("/", (req, res) => {
   const { text, done } = req.body;
   console.log("req.body : ", req.body);
   todoList.push({
